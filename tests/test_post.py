67550d30d7d2405f406eca03f8f8479f432c5e22
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2018-12-28 13:03:15 +1100

    Image parsing from reddit post

--- tests/test_post.py
+++ tests/test_post.py
@@ -41,6 +41,65 @@ def test_factory(input, expected_class):
     assert isinstance(post_factory(input), expected_class)
 
 
+@pytest.mark.parametrize(
+    "input,expected_behavior",
+    [
+        (
+            MockPost(
+                "arg698",
+                "Dispatch Maknae Christmas Party",
+                "[Behind-The-Scenes]",
+                "https://imgur.com/a/rpd1C4y",
+            ),
+            True,
+        ),
+        (
+            MockPost(
+                "asdurr",
+                "Sorn - Happy Mothers Day",
+                "[Behind-The-Scenes]",
+                "https://www.youtube.com/watch?v=URPtbtkjmUA",
+            ),
+            False,
+        ),
+    ],
+)
+def test_media_behavior(input, expected_behavior):
+    assert post_factory(input)._with_media == expected_behavior
+
+
+@pytest.mark.parametrize(
+    "input,expected",
+    [
+        (
+            MockPost(
+                "arg698",
+                "Dispatch Maknae Christmas Party",
+                "[Behind-The-Scenes]",
+                "https://imgur.com/a/rpd1C4y",
+            ),
+            "https://i.imgur.com/L4LnTHD.jpg",
+        ),
+        (
+            MockPost(
+                "asdurr",
+                "Sorn - Happy Mothers Day",
+                "[Behind-The-Scenes]",
+                "https://www.youtube.com/watch?v=URPtbtkjmUA",
+            ),
+            None,
+        ),
+    ],
+)
+def test_media_url(input, expected):
+    assert post_factory(input).image_url == expected
+
+
+"""
+Tests if Base classes are instantiable or not.
+"""
+
+
 @pytest.mark.parametrize("input", [MockPost("abc123", "Test Post", "Post")])
 def test_instantiate_base_class(input):
     with pytest.raises(TypeError):
