d4ad44f91df6bc3407b52831f1c1a6518c06fff5
Jonathan Nicholas <Jonathan.Nicholas@protonmail.com>
2018-12-28 16:08:56 +1100

    main.py fix

--- __main__.py
+++ __main__.py
@@ -1,4 +1,31 @@
-from reddit_streaming import main
+from kpopbot import (
+    PRAWWrapper,
+    TwitterWrapper,
+    Database,
+    post_factory,
+    WHITELIST,
+)
+from CONFIG import REDDIT_CONFIG, TWITTER_CONFIG, DB_URL, TABLE_NAME
 
-if __name__ == '__main__':
+
+def main():
+    reddit = PRAWWrapper(REDDIT_CONFIG)
+    twitter = TwitterWrapper(TWITTER_CONFIG)
+    database = Database(DB_URL, TABLE_NAME)
+    while True:
+        posts = (
+            post_factory(post)
+            for post in reddit.obtain_posts()
+            if database.check_table(post.id)
+            and (
+                post.link_flair_text is not None
+                and post.link_flair_text in WHITELIST
+            )
+        )
+        for post in posts:
+            twitter.tweet(post)
+            database.write_table(post.id)
+
+
+if __name__ == "__main__":
     main()
